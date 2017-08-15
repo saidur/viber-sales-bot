fa819b357c4ccbbe498336111b6f6cfbd3a4c46c
bijon <thinker.bijon@gmail.com>
2017-08-15 15:10:10 +0600

    modify src

--- src/index.js
+++ src/index.js
@@ -460,10 +460,10 @@ if (process.env.NOW_URL || process.env.HEROKU_URL || WEB_URL) {
     const http = require('http');
     const port = process.env.PORT || 5000;
     console.log('Magic happens on port ' + port);
-    app.use("/viber/webhook", bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL||WEB_URL));;
+    //app.use("/viber/webhook", bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL||WEB_URL));;
     //app.listen(port);
     console.log('Magic happens on port ' + port);
-    //http.createServer(bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL||WEB_URL));
+ http.createServer(bot.middleware()).listen(port, () => bot.setWebhook(process.env.NOW_URL || process.env.HEROKU_URL||WEB_URL));
 } else {
     logger.debug('Could not find the now.sh/Heroku environment variables. Please make sure you followed readme guide.');
 }
