bf4acbdfb95d5a3fe2306735ecb122bd022cc60e
bijon <thinker.bijon@gmail.com>
2017-08-15 09:57:05 +0600

    modify src index

--- src/index.js
+++ src/index.js
@@ -366,6 +366,46 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     if (!(message instanceof TextMessage)) {
         say(response, 'Sorry. I can only understand text messages.');
     }
+    const SAMPLE_KEYBOARD = {
+        "Type": "keyboard",
+        "Revision": 1,
+        "Buttons": [
+            {
+                "Columns": 2,
+                "Rows": 2,
+               // "BgColor": "#e6f5ff",
+               // "BgMedia": "http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg",
+               // "BgMediaType": "picture",
+                "BgLoop": true,
+                "ActionType": "reply",
+                "ActionBody": "IT",
+                "Text":"IT",
+                "TextVAlign": "middle",
+                "TextHAlign": "center",
+                "TextOpacity": 60,
+                "TextSize": "regular"
+            },
+            {
+                "Columns": 2,
+                "Rows": 2,
+               // "BgColor": "#e6f5ff",
+               // "BgMedia": "http://www.jqueryscript.net/images/Simplest-Responsive-jQuery-Image-Lightbox-Plugin-simple-lightbox.jpg",
+               // "BgMediaType": "picture",
+                "BgLoop": true,
+                "ActionType": "reply",
+                "ActionBody": "Bank",
+                "Text":"Bank",
+                "TextVAlign": "middle",
+                "TextHAlign": "center",
+                "TextOpacity": 60,
+                "TextSize": "regular"
+            }
+        ]
+    };
+    
+    const keyboardMessage = new KeyboardMessage(SAMPLE_KEYBOARD);
+
+
 });
 
 
